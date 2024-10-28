a0de822acaa741278cdd4d3c4c46ef9c1f050475
Ma <z2715674@gmail.com>
2024-10-28 15:29:24 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# PTT_LOL_Crawler
+
+### 使用 Python 爬取 PTT LOL 版資料並轉成 JSON 以及 Excel 格式
+
+### 套件使用:BeautifulSoup、pandas
+
+<img src="img/lol_index.png" width="70%">
+
+<img src="img/lol_excel.png" width="70%">
---
+++ ptt_lol_excel.py
@@ -0,0 +1,40 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+url = "https://www.ptt.cc/bbs/LoL/index.html"
+
+headers = {
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36",
+}
+request = requests.get(url, headers=headers)
+response = request.text
+soup = BeautifulSoup(response, "html.parser")
+acticles = soup.find_all("div", class_="r-ent")
+data_list = []
+
+for a in acticles:
+    data = {}
+    title = a.find("div", class_="title")
+    if title and title.a:
+        title = title.a.text
+    else:
+        title = "No title"
+    data["標題"] = title
+    popular = a.find("div", class_="nrec")
+    if popular and popular.span:
+        popular = popular.span.text
+    else:
+        popular = "N/A"
+    data["人氣"] = popular
+
+    date = a.find("div", class_="date")
+    if date:
+        date = date.text
+    else:
+        date = "N/A"
+    data["日期"] = date
+    data_list.append(data)
+df = pd.DataFrame(data_list)
+df.to_excel("ptt_lol.xlsx", index=False, engine="openpyxl")
+print("已儲存為.xlsx格式")
